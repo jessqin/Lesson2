7d4777311a3ce714921e7780693575ea6e5ece8e
Jessica N Qin <jq210@pvhs-lab112-d15.pvsd.org>
2016-09-08 14:29:30 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //to declare a variable you:
+        //String or int or double
+        //Example:
+        String s;
+        s = "name";//initialize you have to declare before initializing
+        String s2 = "nickname";
+        System.out.print(s);
+        //Naming Variables:
+            //1. can't name after reserved words
+            //2. can't start it with a number
+            //3. can't use special characters
+            //4. can't hae spaces
+            //lowercase followed by uppercase
+            //5. can't reuse variable name
+        //String vs. int
+        /* int vs double*/
+        int a = 45;
+        double b = 3;
+        double sum = a + b;
+        int e,d = 0;
+        String something = "";
+        //strings have methods meaning they are more advanced
+
+    }
+}
